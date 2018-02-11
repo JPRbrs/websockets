158e0e67ee7d9b4043f3dcf7b09730e83a4bbb46
Javier# <javier@tuware.com>
2018-02-11 22:41:21 +0000

    Move motors with serial messages

---
+++ arduino/move_on_serial_read/move_on_serial_read.ino
@@ -0,0 +1,51 @@
+#define MOTOR 5 // pin 1 chip
+#define DIRA 3 // pin 7 chip
+#define DIRB 4 // pin 2 chip
+#define MOTOR2 10 // pin 10 chip
+#define DIRC 8 // pin 15 chip
+#define DIRD 9 // pin 9 chip
+ // pins 4, 5, 12, 13 GND
+ // pins 8 y 16 to 5V
+ // pin 3 left motor
+ // pin 6 left motor
+ // pin 11 right motor
+ // pin 14 right motor
+
+enum state_t { stateStopped, stateRunning };
+state_t state;
+unsigned long startTime;
+int i;
+int incomingByte;
+ 
+void setup() {
+  //---set pin direction
+  pinMode(MOTOR,OUTPUT);
+  pinMode(MOTOR2, OUTPUT);
+  pinMode(DIRA,OUTPUT);
+  pinMode(DIRC, OUTPUT);
+  pinMode(DIRB,OUTPUT);
+  pinMode(DIRD, OUTPUT);
+  Serial.begin(9600);
+  state = stateStopped;
+  startTime = millis();
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print(incomingByte);
+    Serial.println("Received, going forward");
+    digitalWrite(MOTOR,HIGH);
+    digitalWrite(MOTOR2, HIGH);
+    //    for (i=0;i<5;i++) {
+    digitalWrite(DIRA,LOW); //forward
+    digitalWrite(DIRC, LOW);
+    digitalWrite(DIRB,HIGH);
+    digitalWrite(DIRD, HIGH);
+    delay(400);
+    digitalWrite(MOTOR,LOW);
+    digitalWrite(MOTOR2, LOW);
+    }
+    
+}
